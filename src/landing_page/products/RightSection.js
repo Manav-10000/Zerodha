0ffa5f4ffded363973a52155fffb332fe9b50fe5
Manav-10000 <manavsharma10000@gmail.com>
2024-10-20 21:02:42 +0530

    msg

--- src/landing_page/products/RightSection.js
+++ src/landing_page/products/RightSection.js
@@ -1,7 +1,24 @@
 import React, { PureComponent } from 'react';
-function RightSection() {
+function RightSection({imageURL, productName, productDescription, learnMore}) {
     return ( 
-        <h1>RightSection</h1>
+        <div className="container p-5 mt-5">
+      <div className="row">
+        
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={learnMore}>
+              Learn More
+            </a>
+          </div>
+          
+        </div>
+        <div className="col-6">
+          <img src={imageURL} />
+        </div>
+      </div>
+    </div>
      );
 }
 
--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -1,8 +1,44 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+  return (
+    <div className="container p-5 mt-5">
+      <div className="row text-center">
+        <h1>The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+        
+        
+        <div className="col-4 p-3 mt-5">
+          <img src="media\images\smallcaseLogo.png"/>
+          <p className="text-small text-muted">Thematic investment platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+        <img src="media\images\streakLogo.png"/>
+        <p className="text-small text-muted">Algo & strategy platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+        <img src="media\images\sensibullLogo.svg"/>
+        <p className="text-small text-muted">Options trading platform</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media\images\zerodhaFundhouse.png"/>
+          <p className="text-small text-muted">Asset management</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+        <img src="media\images\goldenpiLogo.png"/>
+        <p className="text-small text-muted">Bonds trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+        <img src="media\images\dittoLogo.png"/>
+        <p className="text-small text-muted">Insurance</p>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
